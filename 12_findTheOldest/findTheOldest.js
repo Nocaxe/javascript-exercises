2af831382e5e11a062bf1e3e5d32015aac9cb224
Nocaxe <123151145+Nocaxe@users.noreply.github.com>
2024-06-30 14:53:10 +0800

    Complete Assignment

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,32 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const date = new Date();
+    const year = date.getFullYear();
+    aAge = 0;
+    bAge = 0;
+    people.sort((a, b) => {
+        if (a.yearOfDeath) {
+            aAge = a.yearOfDeath - a.yearOfBirth;
+        }
+        else {
+            aAge = year - a.yearOfBirth;
+        }
+        if (b.yearOfDeath) {
+            bAge = b.yearOfDeath - b.yearOfBirth;
+        }
+        else {
+            bAge = year - b.yearOfBirth;
+        }
+        if (aAge > bAge) {
+            return -1;
+        }
+        else if (bAge > aAge) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    })
+    return people[0];
 };
 
 // Do not edit below this line
